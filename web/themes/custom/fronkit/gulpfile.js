fa35208ffbef789a3c3d3170cfe3b64e12f035f2
Riadh Rahmi <rahmiriadh@gmail.com>
2021-03-04 17:25:09 +0100

    add block about us

--- web/themes/custom/fronkit/gulpfile.js
+++ web/themes/custom/fronkit/gulpfile.js
@@ -5,26 +5,26 @@ const browserSync = require('browser-sync').create();
 //compile scss into css
 function style() {
   //1.where is my scss
-  return gulp.src('src/assets/scss/**/*.scss') //gets all files ending with .scss in src/scss
+  return gulp.src('scss/**/*.scss') //gets all files ending with .scss in src/scss
     //2. pass that file through sass compiler
     .pipe(sass().on('error',sass.logError))
     //3. where do I save the compiled css file
-    .pipe(gulp.dest('src/assets/css'))
+    .pipe(gulp.dest('css'))
     //4. stream change to all browsers
     .pipe(browserSync.stream());
 }
 
 function watch() {
-  browserSync.init({
-    server: {
-      baseDir: "./html",
-      index: "./html/index.html"
-    }
-  });
-  gulp.watch('src/assets/scss/**/*.scss', style);
-  gulp.watch('./src/*.html').on('change',browserSync.reload);
-  gulp.watch('./src/assets/js/**/*.js').on('change', browserSync.reload);
+  // browserSync.init({
+  //   server: {
+  //     baseDir: "./html",
+  //     index: "./html/index.html"
+  //   }
+  // });
+  gulp.watch('scss/**/*.scss', style);
+  // gulp.watch('./src/*.html').on('change',browserSync.reload);
+  // gulp.watch('./src/assets/js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.style = style;
+// exports.style = style;
 exports.watch = watch;
