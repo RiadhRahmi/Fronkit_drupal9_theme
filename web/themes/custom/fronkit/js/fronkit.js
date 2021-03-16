7916b79de572ef7be3d76757b4d001c57e4054a5
Riadh Rahmi <rahmiriadh@gmail.com>
2021-03-16 17:22:13 +0100

    style carrousel home

--- web/themes/custom/fronkit/js/fronkit.js
+++ web/themes/custom/fronkit/js/fronkit.js
@@ -13,7 +13,28 @@
   Drupal.behaviors.fronkit = {
     attach: function (context, settings) {
 
-      var swiper = new Swiper('.swiper-container', {
+      new Swiper('.carrousel-swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
+
+
+      new Swiper('.team-swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
+
+      new Swiper('.swiper-container', {
         slidesPerView: 6,
         autoplay: {
           delay: 2500,
@@ -24,4 +45,4 @@
     }
   };
 
-} (jQuery, Drupal));
+}(jQuery, Drupal));
