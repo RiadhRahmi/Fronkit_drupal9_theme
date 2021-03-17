f048e3b1809fbb773314d65660ee959701cf21a1
Riadh Rahmi <rahmiriadh@gmail.com>
2021-03-17 17:43:16 +0100

    style footer

--- web/themes/custom/fronkit/js/fronkit.js
+++ web/themes/custom/fronkit/js/fronkit.js
@@ -13,28 +13,28 @@
   Drupal.behaviors.fronkit = {
     attach: function (context, settings) {
 
-      new Swiper('.carrousel-swiper-container', {
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
+      // new Swiper('.carrousel-swiper-container', {
+      //   navigation: {
+      //     nextEl: '.swiper-button-next',
+      //     prevEl: '.swiper-button-prev',
+      //   },
+      //   pagination: {
+      //     el: '.swiper-pagination',
+      //     clickable: true,
+      //   },
+      // });
+      //
+      //
+      // new Swiper('.team-swiper-container', {
+      //   slidesPerView: 3,
+      //   spaceBetween: 30,
+      //   pagination: {
+      //     el: '.swiper-pagination',
+      //     clickable: true,
+      //   },
+      // });
 
-
-      new Swiper('.team-swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
-
-      new Swiper('.swiper-container', {
+      new Swiper('.partners-swiper-container', {
         slidesPerView: 6,
         autoplay: {
           delay: 2500,
