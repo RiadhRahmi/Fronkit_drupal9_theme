d042f54025a2dea94ca991a3f0c89b5fb48d31fe
Riadh Rahmi <rahmiriadh@gmail.com>
2021-03-26 08:20:29 +0100

    put your commit comment

--- web/themes/custom/fronkit/js/fronkit.js
+++ web/themes/custom/fronkit/js/fronkit.js
@@ -13,18 +13,18 @@
   Drupal.behaviors.fronkit = {
     attach: function (context, settings) {
 
-      // new Swiper('.carrousel-swiper-container', {
-      //   navigation: {
-      //     nextEl: '.swiper-button-next',
-      //     prevEl: '.swiper-button-prev',
-      //   },
-      //   pagination: {
-      //     el: '.swiper-pagination',
-      //     clickable: true,
-      //   },
-      // });
-      //
-      //
+      new Swiper('.carrousel-swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
+
+
 
       new Swiper('.team-swiper-container', {
         slidesPerView: 3,
