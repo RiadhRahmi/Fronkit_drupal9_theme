ff080055e6c23805a99ee206db8b6c44a6026865
Riadh Rahmi <rahmiriadh@gmail.com>
2021-04-14 14:59:52 +0100

    put your commit comment

---
+++ web/modules/custom/mymodule/src/Plugin/Layout/FlexibleLayout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\mymodule\Plugin\Layout;
+
+use Drupal\Core\Layout\LayoutDefault;
+
+/**
+ * A layout from our flexible layout builder.
+ *
+ * @Layout(
+ *   id = "flexible_layout",
+ *   deriver = "Drupal\mymodule\Plugin\Deriver\FlexibleLayoutDeriver",
+ *   admin_label = @Translation("Dynamic layout"),
+ *   category = @Translation("Dynamic layout category"),
+ * )
+ */
+class FlexibleLayout extends LayoutDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(array $regions) {
+    $render_array = parent::build($regions);
+    // Since this is a flexible layout builder, you probably need to do
+    // something special to render the layout, so we override the ::build()
+    // method which is responsible for creating a render array.
+    return $render_array;
+  }
+
+}
